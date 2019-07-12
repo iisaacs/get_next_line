988f0aa5b19b02161221a95f45a7f24d3001396f
Ismael ISAACS <iisaacs@c3r1s9.wethinkcode.co.za>
2019-07-12 12:35:18 +0200

    used arrays instead of lists

--- get_next_line.c
+++ get_next_line.c
@@ -5,150 +5,75 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: iisaacs <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/07/03 10:57:49 by iisaacs           #+#    #+#             */
-/*   Updated: 2019/07/08 13:21:36 by iisaacs          ###   ########.fr       */
+/*   Created: 2019/07/11 09:39:27 by iisaacs           #+#    #+#             */
+/*   Updated: 2019/07/12 12:28:33 by iisaacs          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-/*
-**Function to add node to beginning of list.
-**If the head does not exist. Create new_node.
-**And initialize its member with fd and content.
-**Set the next to NULL.
-**If head points to node. Create new node.
-**Initialize members with fd and content.
-**And set next to previous head. New node is now the head.
-**Return head of list.
-*/
-
-static t_vlst	*add_vlst(t_vlst **head, int fd, char *content)
-{
-	t_vlst *new_node;
-
-	if (!head)
-	{
-		*head = (t_vlst*)malloc(sizeof(t_vlst));
-		(*head)->fd = fd;
-		(*head)->start = content;
-		(*head)->content = content;
-		(*head)->next = NULL;
-	}
-	else
-	{
-		new_node = (t_vlst*)malloc(sizeof(t_vlst));
-		new_node->fd = fd;
-		new_node->start = content;
-		new_node->content = content;
-		new_node->next = (*head);
-		(*head) = new_node;
-	}
-	return ((*head));
-}
-
-/*
-** Check if fd is in list.
-** if it is, initialize 'found' to desired node.
-** return TRUE.
-** Otherwise return FALSE.
-*/
-
-static int		is_fd_lst(int fd, t_vlst *head, t_vlst **found)
-{
-	t_vlst *current;
-
-	current = head;
-	while (current)
-	{
-		if (fd == current->fd)
-		{
-			(*found) = current;
-			return (1);
-		}
-		current = current->next;
-	}
-	return (0);
-}
-
-/*
-**Function to copy line from string (*data).
-**And to update the string pointer to after
-**the '\n' character.
-**If line at end of function contains nothing return 0;
-**Else return 1
-*/
-
-static int		cpy_upd(char **data, char **line)
-{
-	char	*str;
-	int		len;
-
-	str = (*data);
-	if ((*str) == '\n')
-	{
-		(*data) = ++str;
-		(*line) = "";
-		return (1);
-	}
-	while (*str != '\n' && (*str))
-		str++;
-	len = str - (*data);
-	if (!((*line) = (char *)malloc(sizeof(char) * (len + 1))))
-		line = NULL;
-	ft_bzero((*line), (size_t)(len + 1));
-	(*line) = ft_strncpy((*line), (*data), len);
-	if (ft_strlen((*line)) == 0)
-		return (0);
-	if ((*str) == '\n')
-		str++;
-	(*data) = str;
-	return (1);
-}
-
-static char		*read_buff(int fd, char *buff, char *new_str, int *ret)
+static char	*cpy_upd(char **line, char **str)
 {
 	char	*temp;
+	int		len;
 
-	temp = NULL;
-	new_str = NULL;
-	while ((*ret = read(fd, buff, BUFF_SIZE)))
+	temp = (*str);
+	if (ft_strlen(*str) == 0)
+		return (NULL);
+	if ((*temp) == '\n')
 	{
-		if (*ret < 0)
-			return (NULL);
+		(*str)++;
+		return ("");
+	}
+	while ((*temp) != '\n' && (*temp) != '\0')
+		temp++;
+	len = (temp) - (*str);
+	if (!(*line = (char *)malloc(len + 1)))
+		return (NULL);
+	ft_bzero(*line, (len + 1));
+	(*line) = ft_strncpy((*line), (*str), len);
+	if ((*temp) == '\n')
+		(*str)++;
+	(*str) += len;
+	return ((*line));
+}
+
+static char	*read_buff(char *buff, const int fd)
+{
+	char *temp;
+	char *new_str;
+
+	new_str = NULL;
+	while (read(fd, buff, BUFF_SIZE))
+	{
 		temp = new_str;
 		new_str = (!new_str) ? ft_strdup((const char*)buff) :
 			ft_strjoin(new_str, (const char*)buff);
 		free(temp);
-		ft_bzero(buff, *ret);
+		ft_bzero(buff, BUFF_SIZE + 1);
 	}
 	return (new_str);
 }
 
-int				get_next_line(const int fd, char **line)
+int			get_next_line(const int fd, char **line)
 {
-	char			*buff[BUFF_SIZE + 1];
-	char			*new_str;
-	static	t_vlst	*head;
-	t_vlst			*found;
-	int				ret;
+	static char *all_files[654321];
+	char		buff[BUFF_SIZE + 1];
+	static char	*p;
 
-	new_str = NULL;
+	p = NULL;
 	ft_bzero(buff, BUFF_SIZE + 1);
-	if (fd < 0 || !line || BUFF_SIZE < 1)
+	if (fd < 0 || !line || BUFF_SIZE < 1 || (read(fd, buff, 0) == -1))
 		return (-1);
-	if (!is_fd_lst(fd, head, &found))
+	if (!all_files[fd])
 	{
-		new_str = read_buff(fd, (char *)buff, new_str, &ret);
-		if (ret < 0)
-			return (-1);
-		if (!new_str)
-			return (0);
-		found = add_vlst(&head, fd, new_str);
+		all_files[fd] = read_buff(buff, fd);
+		p = all_files[fd];
 	}
-	if (!(cpy_upd(&(found->content), line)))
+	(*line) = cpy_upd(line, &all_files[fd]);
+	if (!(*line))
 	{
-		free(found->start);
+		free(p);
 		return (0);
 	}
 	return (1);
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: iisaacs <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/20 08:19:07 by iisaacs           #+#    #+#             */
-/*   Updated: 2019/07/08 13:19:14 by iisaacs          ###   ########.fr       */
+/*   Updated: 2019/07/11 16:15:41 by iisaacs          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,7 @@
 # include <stdio.h>
 # include <unistd.h>
 # include "libft/includes/libft.h"
-# define BUFF_SIZE 1
-
-typedef struct		s_vlst
-{
-	int				fd;
-	char			*start;
-	char			*content;
-	struct s_vlst	*next;
-}					t_vlst;
+# define BUFF_SIZE 42
 
 int					get_next_line(const int fd, char **line);
 
